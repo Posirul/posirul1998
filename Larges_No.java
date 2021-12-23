d949cbb5a5a684e62323a509c4e8f62d83135865
Posirul <96307535+Posirul@users.noreply.github.com>
2021-12-23 20:26:08 +0530

    Add files via upload

---
+++ Larges_No.java
@@ -0,0 +1,21 @@
+package FirstHand_D_23;
+
+public class Larges_No {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int num1=100;
+     int num2=200;
+     int num3=300;
+     if(num1>num2) {
+    	 System.out.println(num1);
+     }
+     else if(num2>num3) {
+    	 System.out.println(num2);
+     }
+     else {
+    	 System.out.print(num3);
+     }
+	}
+
+}
