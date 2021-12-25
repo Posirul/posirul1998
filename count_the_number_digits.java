d3a547bfdf9304150ded2288a5f640a6aa47e5ef
Posirul <96307535+Posirul@users.noreply.github.com>
2021-12-25 09:36:11 +0530

    Add files via upload

---
+++ count_the_number_digits.java
@@ -0,0 +1,20 @@
+package FirstHand_D_25;
+
+public class count_the_number_digits {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int num = 586245;
+      int count = 0;
+      while(num!=0);
+      num=num/10;
+      {
+    	  count++;
+      }
+      {
+    	  System.out.println(count++);
+      }
+      
+	}
+
+}
