671427607b8a781f001f8209df214f3b0b4c7076
Posirul <96307535+Posirul@users.noreply.github.com>
2021-12-25 09:39:57 +0530

    Add files via upload

---
+++ perfect_square.java
@@ -0,0 +1,13 @@
+package FirstHand_D_25;
+
+public class perfect_square {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int i=8;
+        for(i=1;i<=7;i=i+1);
+        System.out.println(i*i);
+        	
+	}
+
+}
